9324f78ec22131139364f5f662575d9d179752a5
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-30 13:50:40 +0100

    property:property:[name1]:[name2] is broken
    Yet it doesn't throw but use only name1 with 'literal' options
    Must be fixed in keyfunc package.

--- test/advanced-property.test.js
+++ test/advanced-property.test.js
@@ -90,6 +90,37 @@ describe('Testing Singleton with property', function() {
   });
 
   it(`Calling SingletonFactory(Class, [{
+    type: 'property:property:id:id',
+    property: 'data'
+  }])`, function() {
+    // Checking that this option doesn't work as intended
+
+    class Class {constructor() {}}
+
+    const Singleton = SingletonFactory(Class, [{
+      type: 'property:property:id:id',
+      property: 'data'
+    }]);
+
+    const o1 = {id: {id: 1}};
+    const o2 = {id: {id: 2}};
+
+    const c1 = {data: o1};
+    const c2 = {data: o2};
+    const c3 = {data: o1};
+
+    const s1 = Singleton(c1);
+    const s2 = Singleton(c2);
+    const s3 = Singleton(c3);
+
+    expect(s1).not.to.equal(s2);
+    expect(s1).to.equal(s3);
+    expect(Singleton.key(s1)).not.to.equal(signature(1)); // Bad!!!
+    expect(Singleton.key(s1)).to.equal(signature({id: 1})); // Bad!!!
+
+  });
+
+  it(`Calling SingletonFactory(Class, [{
     type: 'property:array',
     property: 'data'
   }])`, function() {
