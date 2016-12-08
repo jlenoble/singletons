6fb99750cee1e18b43735e100d8340de2d821bb5
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-08 11:38:14 +0100

    Init with [singleton]
    Another special case where the init args have been preprocessed to have
    array form. We want in such a case to just return the singleton.

--- test/type.test.js
+++ test/type.test.js
@@ -70,4 +70,48 @@ describe('Testing Singleton with Type initialization', function() {
     expect(Singleton2(t2)).to.equal(Singleton2(t));
   });
 
+  it(`Calling two different Singletons with [same Type object]`, function() {
+
+    class Class {constructor() {}}
+
+    const Singleton1 = SingletonFactory(Class, [{stem: 'ONE'}]);
+    const Singleton2 = SingletonFactory(Class, [{stem: 'TWO'}]);
+
+    const c = new Class();
+    expect(Singleton1.key(c)).to.equal('ONE1');
+    expect(Singleton2.key(c)).to.equal('TWO1');
+
+    const s = Singleton1(c);
+    expect(s).not.to.equal(c);
+    expect(Singleton1.key(s)).to.equal('ONE1');
+    expect(Singleton2.key(s)).to.equal('TWO2');
+
+    const t = Singleton2(c);
+    expect(t).not.to.equal(c);
+    expect(t).not.to.equal(s);
+    expect(Singleton1.key(t)).to.equal('ONE2');
+    expect(Singleton2.key(t)).to.equal('TWO1');
+
+    const s1 = Singleton1([s]);
+    expect(s1).to.equal(s);
+
+    const t2 = Singleton2([t]);
+    expect(t2).to.equal(t);
+
+    const s2 = Singleton2(s);
+    expect(s2).not.equal(s);
+    expect(Singleton1.key(s2)).to.equal('ONE3');
+    expect(Singleton2.key(s2)).to.equal('TWO2');
+
+    const t1 = Singleton1(t);
+    expect(t1).not.equal(t);
+    expect(Singleton1.key(t1)).to.equal('ONE2');
+    expect(Singleton2.key(t1)).to.equal('TWO3');
+
+    expect(Singleton1([s1])).to.equal(Singleton1([s]));
+    expect(Singleton1([t1])).to.equal(Singleton1(t));
+    expect(Singleton2([s2])).to.equal(Singleton2(s));
+    expect(Singleton2([t2])).to.equal(Singleton2([t]));
+  });
+
 });
